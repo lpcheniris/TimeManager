b1a835b332d62967c1e7ca971a88dd608c35ab18
lpchen <lpcheniris@gmail.com>
2020-09-21 18:00:37 +0800

    fix Timer Model

--- Server/src/models/Timer.ts
+++ Server/src/models/Timer.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import { Plane } from "./Plane";
+import { Schedule } from "./Schedule";
 const TimerSchema = new mongoose.Schema({
   durationTime: { type: Number, min: 0, required: true },
   endTime: { type: Date, required: true },
   startTime: { type: Date, required: true },
-  plane: { type: mongoose.Schema.Types.ObjectId, ref: Plane,required: true },
-  schedule: { type: String, required: true, minlength:2 , maxlength: 15}
+  plane: { type: mongoose.Schema.Types.ObjectId, ref: Plane, required: true },
+  schedule: { type: mongoose.Schema.Types.ObjectId, ref: Schedule, required: true}
 })
 
 export const Timer = mongoose.model("Timer", TimerSchema)
