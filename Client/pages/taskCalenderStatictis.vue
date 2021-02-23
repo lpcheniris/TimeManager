c7cae324042759aa87f4b1abe50cd1b83a6085b6
lpchen <lpcheniris@gmail.com>
2021-02-23 19:39:26 +0800

    Add task calendar for task list

--- Client/pages/taskCalenderStatictis.vue
+++ Client/pages/taskCalenderStatictis.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-      <div class="count-container">
-       <div class="count-item" :key="taskItem.task" v-for="taskItem in taskTimes">
-           <div>{{ taskItem.task }}</div>
-           <div>{{ taskItem.count}} Times</div>
-       </div>
+    <div class="count-container">
+      <div
+        class="count-item"
+        :key="taskItem.task"
+        v-for="taskItem in taskTimes"
+      >
+        <div>{{ taskItem.task }}</div>
+        <div>{{ taskItem.count }} Times</div>
       </div>
+    </div>
+    <div class="calendar-container">
+      <a-calendar :fullscreen="true" :header-render="headerRender">
+        <div
+          class="task-date-label"
+          slot="dateCellRender"
+          slot-scope="value"
+          :style="{ 'background-color': calculateTaskColor(value) }"
+          v-if="calculateTaskDisplay(value)"
+        ></div>
+      </a-calendar>
+    </div>
   </div>
 </template>
 
@@ -19,8 +34,8 @@ export default {
   name: "taskCalenderStatictis",
   data() {
     return {
-      startTime: moment().subtract(100, "day"),
-      endTime: moment(),
+      startTime: moment().startOf("month"),
+      endTime: moment().endOf("month"),
       taskCalendarListByMonth: [],
       tasks: [],
     };
@@ -28,7 +43,7 @@ export default {
   computed: {
     taskTimes: function () {
       let taskObject = {};
-      if (this.tasks.length && this.taskCalendarListByMonth.length) {
+      if (this.checkTaskValiable()) {
         this.tasks.forEach((v, i) => {
           taskObject[v._id] = {
             task: v.task,
@@ -62,16 +77,66 @@ export default {
       this.tasks = res.data.data;
     });
   },
-  methods: {},
+  methods: {
+    checkTaskValiable() {
+      return this.tasks.length && this.taskCalendarListByMonth.length;
+    },
+    calculateTaskColor(date) {
+      if (this.checkTaskValiable()) {
+        let taskList = _.find(this.taskCalendarListByMonth, (v) => {
+          return (
+            moment(v.date).format("YYYY-MM-DD") == date.format("YYYY-MM-DD")
+          );
+        });
+        let planeTaskLength = this.tasks.length;
+        let taskListLength = taskList ? taskList.tasks.length : 0;
+        let opacity = planeTaskLength ? (taskListLength / planeTaskLength) + 0.2 : 0.2
+        console.log((taskListLength / planeTaskLength))
+
+        return `rgba(100, 149, 237, ${opacity})`
+      }
+    },
+    calculateTaskDisplay(date) {
+      return date.isBefore(this.endTime) && date.isAfter(this.startTime)
+    },
+    headerRender() {
+      return null;
+    },
+  },
 };
 </script>
 
 <style>
+.ant-fullcalendar-fullscreen .ant-fullcalendar-column-header {
+    padding-right: 18px;
+}
+.ant-fullcalendar-fullscreen .ant-fullcalendar-value { 
+   text-align: center;
+}
+.ant-fullcalendar .ant-fullcalendar-content {
+  height: auto;
+}
+.ant-fullcalendar-fullscreen .ant-fullcalendar-date {
+  border-top: 0;
+  height: auto;
+}
+.task-date-label {
+  width: 100%;
+  height: 20px;
+  border-radius: 3px;
+  /* border: 1px  solid; */
+}
+.calendar-container {
+  padding: 10px;
+}
 .count-item {
-    display: flex;
-    justify-content: space-between;
-    padding: 5px 20px;
-    border-bottom: 1px solid #ccc;
-    margin: 5px 10px;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 20px;
+  border-bottom: 1px solid #ccc;
+  margin: 5px 10px;
+}
+.count-container {
+  height: 300px;
 }
 </style>
