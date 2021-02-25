39875fb239e2759d03064815ff8647dc23074be5
lpchen <lpcheniris@gmail.com>
2021-02-25 17:24:18 +0800

    fix ResetUpDB

--- Server/src/utils/ResetupDB.ts
+++ Server/src/utils/ResetupDB.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose"
 
 import setupMongo from "../config/mongo";
-import { Plan } from "../models/Plan"
+// import { Plan } from "../models/Plan"
 import { Timer } from "../models/Timer"
 import { Schedule } from "../models/Schedule"
+// import { Task } from "../models/Task";
+import { TaskCalendar } from "../models/TaskCalendar";
 
 setupMongo() 
 
@@ -11,6 +13,9 @@ async function addPlans() {
   try {
     await Timer.remove({})
     await Schedule.remove({})
+    // await Plan.remove({})
+    // await Task.remove({})
+    await TaskCalendar.remove({})
     console.log("Remove Timer and Shedules Seccefully!")
   } catch(e) {
     console.error(e)
